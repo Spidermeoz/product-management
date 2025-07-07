a06ebb0049d65ca80d74c7d23aba752f36360304
SpiderMeow <thoconditonglao123@gmail.com>
2025-07-07 18:15:57 +0700

    Kết nối mongoose

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const database = require('./config/database');
+
 require('dotenv').config();
 
 const route = require('./routes/client/index.route');
 
 const app = express();
 const port = process.env.PORT;
+
+database.connect();
 
 app.set('views', './views');
 app.set('view engine', 'pug');
