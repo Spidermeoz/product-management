018c43f26485728a244d47f6d3d73ba40fd053d0
SpiderMeow <thoconditonglao123@gmail.com>
2025-07-08 19:04:58 +0700

    Làm giao diện danh sách sản phẩm admin

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const database = require('./config/database');
 
+const systemConfig = require('./config/system');
+
 require('dotenv').config();
 
 const route = require('./routes/client/index.route');
+const routeAdmin = require('./routes/admin/index.route');
 
 const app = express();
 const port = process.env.PORT;
@@ -13,9 +16,13 @@ database.connect();
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+// App local variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
 app.use(express.static('public'));
 
 //routes
+routeAdmin(app);
 route(app);
 
 app.listen(port, () => {
