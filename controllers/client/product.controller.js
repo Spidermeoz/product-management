dac46bc154891452303b65689267358d48d584c4
SpiderMeow <thoconditonglao123@gmail.com>
2025-07-08 16:33:02 +0700

    Làm trang danh sách sản phẩm bên Client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,22 @@
-module.exports.index = (req, res) => {
-  res.render("client/pages/products/index", {
-    pageTitle: "Trang danh sách sản phẩm"
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+  console.log("Product controller index called");
+  const products = await Product.find({
+    status: "active", // Lấy tất cả sản phẩm có trạng thái active
+    deleted: false // Lấy tất cả sản phẩm chưa bị xóa
   });
+
+  const newProducts = products.map(item => {
+    item.priceNew = item.price - (item.price * item.discountPercentage / 100);
+    item.priceNew = item.priceNew.toFixed(2); // Làm tròn đến 2 chữ số thập phân
+    return item;
+  })
+
+  console.log(products); // In ra console để kiểm tra
+
+  res.render("client/pages/products/index", {
+  pageTitle: "Trang danh sách sản phẩm",
+  products: newProducts // Truyền danh sách sản phẩm vào view
+});
 };
