069c9e537e10150598c56c712b123fd517c3c1f0
SpiderMeow <thoconditonglao123@gmail.com>
2025-07-10 00:40:41 +0700

    Tối ưu phân trang (lưu riêng trong file helper và mixins để tái sử dụng)

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -3,6 +3,7 @@ const Product = require("../../models/product.model");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
 
 module.exports.index = async (req, res) => {
   // console.log(req.query.status)
@@ -28,29 +29,37 @@ module.exports.index = async (req, res) => {
   // End of search
 
   //Pagination
-  let objectPagination = {
-    currentPage: 1,
-    limitItems: 6
-  }
-
-  if (req.query.page) {
-    objectPagination.currentPage = parseInt(req.query.page);
-  }
-
-  objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems;
-
   const countProducts = await Product.countDocuments(find);
-  const totalPages = Math.ceil(countProducts / objectPagination.limitItems);
-  objectPagination.totalPages = totalPages;
+
+  let objectPagination = paginationHelper(
+    {
+      currentPage: 1,
+      limitItems: 6,
+    },
+    req.query,
+    countProducts
+  );
+
+  // if (req.query.page) {
+  //   objectPagination.currentPage = parseInt(req.query.page);
+  // }
+
+  // objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems;
+
+  // const countProducts = await Product.countDocuments(find);
+  // const totalPages = Math.ceil(countProducts / objectPagination.limitItems);
+  // objectPagination.totalPages = totalPages;
   //End of Pagination
 
-  const products = await Product.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip);
+  const products = await Product.find(find)
+    .limit(objectPagination.limitItems)
+    .skip(objectPagination.skip);
 
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
     products: products,
     filterStatus: filterStatus,
     keyword: objectSearch.keyword,
-    pagination: objectPagination
+    pagination: objectPagination,
   });
 };
