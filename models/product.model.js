e05f57485170176c76b4a5c60a794497fa01de89
SpiderMeow <thoconditonglao123@gmail.com>
2025-07-11 16:23:34 +0700

    Chức năng tạo sản phẩm mới

--- models/product.model.js
+++ models/product.model.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const slug = require('mongoose-slug-updater');
+
+mongoose.plugin(slug);
 
 const productSchema = new mongoose.Schema({
   title: String,
@@ -9,12 +12,20 @@ const productSchema = new mongoose.Schema({
   thumbnail: String,
   status: String,
   position: Number,
-  deleted: Boolean,
-  deletedAt: {
-    type: Date,
-    default: null,
+  slug: { 
+    type: String, 
+    slug: "title",
+    unique: true
   },
-});
+  deleted: {
+    type: Boolean,
+    default: false
+  },
+  deletedAt: Date
+},{
+  timestamps: true
+}
+);
 
 const Product = mongoose.model("Product", productSchema, "products");
 
