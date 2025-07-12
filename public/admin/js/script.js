f3f9f433a6f5d8477a99f058546af7973caddb19
SpiderMeow <thoconditonglao123@gmail.com>
2025-07-12 12:42:02 +0700

    Thêm preview ảnh trước khi upload

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -143,16 +143,31 @@ if (formChangeMulti) {
 
 // Show alert
 const showAlert = document.querySelector("[show-alert]");
-if (showAlert)  {
+if (showAlert) {
   const time = parseInt(showAlert.getAttribute("data-time"));
-  const closeAlert = showAlert.querySelector("[close-alert]")
+  const closeAlert = showAlert.querySelector("[close-alert]");
 
   setTimeout(() => {
     showAlert.classList.add("alert-hidden");
   }, time);
 
   closeAlert.addEventListener("click", () => {
-    showAlert.classList.add("alert-hidden")
-  })
+    showAlert.classList.add("alert-hidden");
+  });
 }
 // End show alert
+
+// Upload image
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage) {
+  const uploadImageInput = document.querySelector("[upload-image-input]");
+  const uploadImagePreview = document.querySelector("[upload-image-preview]");
+
+  uploadImageInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      uploadImagePreview.src = URL.createObjectURL(file);
+    }
+  });
+}
+// End upload image
