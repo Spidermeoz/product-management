f955ed9ceb3d2a66d054aa27f9e367cb077649cd
SpiderMeow <thoconditonglao123@gmail.com>
2025-07-19 13:24:18 +0700

    Logs lịch sử thêm sản phẩm

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const flash = require('express-flash');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const moment = require('moment');
 
 const database = require('./config/database');
 
@@ -45,6 +46,7 @@ app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce
 
 // App local variables
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
+app.locals.moment = moment;
 
 app.use(express.static(`${__dirname}/public`));
 
