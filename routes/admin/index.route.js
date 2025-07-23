de49ef67ad16b54ff3a813e91475906f1c689a34
SpiderMeow <thoconditonglao123@gmail.com>
2025-07-23 18:30:17 +0700

    Phần cài đặt chung bên admin

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,11 +1,14 @@
-const dashboardRoutes = require("./dashboard.route");
 const systemConfig = require("../../config/system");
+
+const dashboardRoutes = require("./dashboard.route");
 const productRoutes = require("./product.route");
 const productCategoryRoutes = require("./product-category.route");
 const roleRoutes = require("./role.route");
 const accountRoutes = require("./account.route");
 const authRoutes = require("./auth.route");
 const myAccountRoutes = require("./my-account.route");
+const settingRoutes = require("./setting.route");
+
 const authMiddleware = require("../../middlewares/admin/auth.middleware");
 
 
@@ -34,5 +37,6 @@ module.exports = (app) => {
 
   app.use(`${PATH_ADMIN}/my-account`, authMiddleware.requireAuth, myAccountRoutes);
 
-  
+  app.use(`${PATH_ADMIN}/settings`, authMiddleware.requireAuth, settingRoutes);
+
 };
