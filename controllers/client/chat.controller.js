9cfb941bade76ab7666e6b3c79d4c2817c9d2a29
SpiderMeow <thoconditonglao123@gmail.com>
2025-07-23 23:39:49 +0700

    Làm tính năng chat cơ bản

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,14 +1,37 @@
+const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model");
+
 // [GET] /chat
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+  const userId = res.locals.user.id;
   // SocketIO
-  _io.on("connection", (socket) => {
-    console.log("a user connected", socket.id);
-    // socket.on("disconnect", () => {
-    //   console.log("user disconnected");
-    // });
+  _io.once("connection", (socket) => {
+    socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+      // Lưu vào database
+      const chat = new Chat({
+        user_id: userId,
+        content: content,
+      });
+      await chat.save();
+    });
   });
   // End SocketIO
+
+  // Lấy data từ database
+  const chats = await Chat.find({
+    deleted: false,
+  });
+
+  for (const chat of chats) {
+    const infoUser = await User.findOne({
+      _id: chat.user_id,
+    }).select("fullName");
+    chat.infoUser = infoUser;
+    console.log(infoUser)
+  }
+  // Hết Lấy data từ database
   res.render("client/pages/chat/index", {
     pageTitle: "Chat",
+    chats: chats,
   });
 };
