a9724821a83c550c1513785fb5e3cfb3ffb2bbd7
SpiderMeow <thoconditonglao123@gmail.com>
2025-07-24 22:36:18 +0700

    Chức năng gửi yêu cầu kết bạn

--- controllers/client/users.controller.js
+++ controllers/client/users.controller.js
@@ -1,14 +1,33 @@
 const User = require("../../models/user.model");
+const usersSocket = require("../../sockets/client/users.socket")
 
 // [GET] /user/not-friend
 module.exports.notFriend = async (req, res) => {
+  // SocketIO
+    usersSocket(res)
+    // End SocketIO
   const userId = res.locals.user.id;
 
+  const myUser = await User.findOne({
+    _id: userId
+  })
+  console.log(myUser)
+
+  const requestFriends = myUser.requestFriends
+  const acceptFriends = myUser.acceptFriends
+
+  console.log(requestFriends, acceptFriends)
+
   const users = await User.find({
-    _id: { $ne: userId },
+    $and: [
+      {_id: { $ne: userId }},
+      {_id: { $nin: requestFriends }},
+      {_id: { $nin: acceptFriends }}
+    ],
     status: "active",
     deleted: false,
   }).select("id avatar fullName");
+  console.log(users)
 
   res.render("client/pages/users/not-friend.pug", {
     pageTitle: "Danh sách người dùng",
---
+++ sockets/client/users.socket.js
@@ -0,0 +1,48 @@
+const User = require("../../models/user.model");
+
+module.exports = (res) => {
+  // SocketIO
+  _io.once("connection", (socket) => {
+    socket.on("CLIENT_ADD_FRIEND", async (userId) => {
+      const myUserId = res.locals.user.id;
+
+      console.log(myUserId);
+      console.log(userId);
+      // Thêm id của A và acceptFriends của B
+      existIdAinB = await User.findOne({
+        _id: userId,
+        acceptFriends: myUserId,
+      });
+      if (!existIdAinB) {
+        await User.updateOne(
+          {
+            _id: userId,
+          },
+          {
+            $push: { acceptFriends: myUserId },
+          }
+        );
+      }
+      // End Thêm id của A và acceptFriends của B
+
+      // Thêm id của B vào requestFriends của A
+      existIdBinA = await User.findOne({
+        _id: myUserId,
+        acceptFriends: userId,
+      });
+      if (!existIdBinA) {
+        await User.updateOne(
+          {
+            _id: myUserId,
+          },
+          {
+            $push: { requestFriends: userId },
+          }
+        );
+      }
+      // End Thêm id của B vào requestFriends của A
+    });
+    // End typing
+  });
+  // End SocketIO
+};
