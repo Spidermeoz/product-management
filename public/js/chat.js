e88a4784091630964d946acd710db47597b772a8
SpiderMeow <thoconditonglao123@gmail.com>
2025-07-24 12:24:27 +0700

    Hiển thị Typing (Hoàn thiện)

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,6 +9,7 @@ if (formSendData) {
     if (content) {
       socket.emit("CLIENT_SEND_MESSAGE", content);
       e.target.elements.content.value = "";
+      socket.emit("CLIENT_SEND_TYPING", "hidden");
     }
   });
 }
@@ -19,6 +20,7 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
   const myId = document.querySelector("[my-id]").getAttribute("my-id");
   const body = document.querySelector(".chat .inner-body");
   const div = document.createElement("div");
+  const boxTyping = document.querySelector(".chat .inner-list-typing");
   let htmlFullName = "";
 
   if (myId == data.user_id) {
@@ -31,7 +33,7 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
     ${htmlFullName}
     <div class="inner-content">${data.content}</div>
   `;
-  body.appendChild(div);
+  body.insertBefore(div, boxTyping);
 
   bodyChat.scrollTop = bodyChat.scrollHeight;
 });
@@ -58,7 +60,21 @@ if (buttonIcon) {
 }
 // End Show popup
 
+// Show Typing
+var timeOut;
+const showTyping = () => {
+  socket.emit("CLIENT_SEND_TYPING", "show");
+
+  clearTimeout(timeOut);
+
+  timeOut = setTimeout(() => {
+    socket.emit("CLIENT_SEND_TYPING", "hidden");
+  }, 3000);
+};
+// End Show Typing
+
 // Insert icon to input
+
 const emojiPicker = document.querySelector("emoji-picker");
 if (emojiPicker) {
   const inputChat = document.querySelector(
@@ -67,10 +83,18 @@ if (emojiPicker) {
   emojiPicker.addEventListener("emoji-click", (event) => {
     const icon = event.detail.unicode;
     inputChat.value = inputChat.value + icon;
+
+    // Setup để con trỏ chuột luôn ở vị trí cuối cùng
+    const end = inputChat.value.length;
+    inputChat.setSelectionRange(end, end);
+    inputChat.focus();
+    // End Setup để con trỏ chuột luôn ở vị trí cuối cùng
+
+    showTyping();
   });
   // Input keyup
   inputChat.addEventListener("keyup", () => {
-    socket.emit("CLIENT_SEND_TYPING", "show");
+    showTyping();
   });
   // End Input keyup
 }
@@ -79,7 +103,42 @@ if (emojiPicker) {
 // End Show icon chat
 
 // SERVER_RETURN_TYPING
-socket.on("SERVER_RETURN_TYPING", (data) => {
-  console.log(data);
-});
+const elementListTyping = document.querySelector(".chat .inner-list-typing");
+
+if (elementListTyping) {
+  socket.on("SERVER_RETURN_TYPING", (data) => {
+    if (data.type === "show") {
+      // Nếu đã có box của user này thì không thêm lại
+      const existTyping = elementListTyping.querySelector(
+        `[user-id="${data.userId}"]`
+      );
+      if (!existTyping) {
+        const bodyChat = document.querySelector(".chat .inner-body");
+        const boxTyping = document.createElement("div");
+        boxTyping.classList.add("box-typing");
+        boxTyping.setAttribute("user-id", data.userId);
+
+        boxTyping.innerHTML = `
+        <div class="inner-name">${data.fullName}</div>
+        <div class="inner-dots">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      `;
+
+        elementListTyping.appendChild(boxTyping);
+        bodyChat.scrollTop = bodyChat.scrollHeight;
+      }
+    } else {
+      const boxTypingRemove = elementListTyping.querySelector(
+        `[user-id="${data.userId}"]`
+      );
+      if (boxTypingRemove) {
+        elementListTyping.removeChild(boxTypingRemove);
+      }
+    }
+  });
+}
+
 // End SERVER_RETURN_TYPING
